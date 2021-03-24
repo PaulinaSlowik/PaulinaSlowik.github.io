9e244863f3f0078fd4ac0af5e0af4599564bdfc1
Paulina <67586415+PaulinaSlowik@users.noreply.github.com>
2021-03-24 21:17:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   const apiRoot = 'https://enigmatic-fjord-41138.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'https://enigmatic-fjord-41138.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://enigmatic-fjord-41138.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
